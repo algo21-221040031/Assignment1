42b284988c3699252ea1b5ed83df9b2c442926bd
algo21-221040031 <221040031@link.cuhk.edu.cn>
2022-03-12 17:21:30 +0800

    Update the LongShortBacktesting Function.

--- BackTesting.py
+++ BackTesting.py
@@ -85,4 +85,76 @@ def HierarchicalBacktesting(data, industry, selected):
     resultMatrix['SharpeRatio'] = resultMatrix['mean'] * \
         52/(resultMatrix['std']*m.sqrt(52))
     finalMatrix = pd.concat([resultMatrix, drawdown], axis=1)
+
+    return finalMatrix
+
+def LongShortBackTesting(data, industry, selected):
+    """
+    :param data: pandas DataFrame, containing all variables and data
+    :param industry: list, containing industry string
+    :param selected: list, containing principle component
+    :return: pandas DataFrame describing the performance of strategy
+
+    """
+    # 总时间长度
+    timePeriod = len(data)
+    # 划分训练集和测试集
+    splitLine = int(0.7*len(data))
+
+    returnList = []
+
+    for i in range(splitLine, timePeriod):
+        # 构建训练集，每个时间截点要往前推一期
+        trainData = data.iloc[i-splitLine:i, :]
+        returnDic = {}
+        for j in industry:
+            # 分别对每个行业依次做主成分回归
+            formula = "{} ~ {} + 1".format(j, "+".join(selected))
+            model = smf.ols(formula, data).fit()
+            params = model.params
+            component = trainData.iloc[-1, -6:]
+            intercept = pd.Series({'Intercept': 1})
+            componentIntercept = component.append(intercept)
+            predictReturn = np.matmul(params, componentIntercept)
+            returnDic[j] = predictReturn
+        returnDf = pd.DataFrame(returnDic, index=[0]).T
+        returnDf.columns = ['Return']
+        # long-short
+        sortedReturnDf = returnDf.sort_values(by='Return', ascending=False)
+        firstReturn = sortedReturnDf['Return'].iloc[0]
+        lastReturn = sortedReturnDf['Return'].iloc[len(sortedReturnDf)-1]
+        netReturn = firstReturn - lastReturn
+        returnList.append(netReturn)
+
+    def NetValue(array):
+        # 计算单位净值
+        x = 1
+        netValue = [1]
+        for i in range(len(array)):
+            x = x * (1 + array[i])
+            netValue.append(x)
+        return netValue
+
+    netValue = NetValue(returnList)
+
+    def get_max_drawdown(array):
+        # 计算最大回撤
+        drawdowns = []
+        for i in range(len(array)):
+            max_array = max(array[:i+1])
+            drawdown = max_array - array[i]
+            drawdowns.append(drawdown)
+        return max(drawdowns)
+
+    drawDown = get_max_drawdown(netValue)
+
+    drawdown = pd.DataFrame({'LongShort': drawDown}, index=[0]).T
+    drawdown.columns = ['Drawdown']
+    longShortDf = pd.DataFrame({'LongShort': returnList})
+    resultMatrix = longShortDf.describe().T
+    # 计算夏普比率
+    resultMatrix['SharpeRatio'] = resultMatrix['mean'] * \
+        52/(resultMatrix['std']*m.sqrt(52))
+    finalMatrix = pd.concat([resultMatrix, drawdown], axis=1)
+
     return finalMatrix
